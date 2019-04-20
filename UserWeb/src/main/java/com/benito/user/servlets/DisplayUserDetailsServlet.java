370a74cedbcd8329b9b0980631ac230b7e799b6c
Branislav Beno <brano.beno@gmail.com>
2019-04-20 23:00:57 +0200

    Code vulnerabilities according to sonar analysis

--- UserWeb/src/main/java/com/benito/user/servlets/DisplayUserDetailsServlet.java
+++ UserWeb/src/main/java/com/benito/user/servlets/DisplayUserDetailsServlet.java
@@ -5,11 +5,11 @@ import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.BeansException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -26,8 +26,7 @@ public class DisplayUserDetailsServlet extends HttpServlet {
 	private static final Logger logger = Logger.getLogger(DisplayUserDetailsServlet.class.getName());
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 		try {
 			response.getWriter().append("Served at: ").append(request.getContextPath());
 
@@ -43,6 +42,10 @@ public class DisplayUserDetailsServlet extends HttpServlet {
 			out.println("User Name:   " + user.getName());
 			out.println("User e-mail: " + user.getMail());
 
+		} catch (NumberFormatException e) {
+			logger.log(Level.SEVERE, "NumberFormatException thrown");
+		} catch (BeansException e) {
+			logger.log(Level.SEVERE, "BeansException thrown");
 		} catch (IllegalStateException e) {
 			logger.log(Level.SEVERE, "IllegalStateException thrown");
 		} catch (IOException e) {
